8dcf6c599c4e59850c7752a2f4f45a4b22c8a23d
Chris Henson <chrishenson.net@gmail.com>
2020-12-03 03:54:23 +0000

    Finished part 2

--- 2016/01/python/01.py
+++ 2016/01/python/01.py
@@ -12,6 +12,8 @@ class compass:
                         2:np.array([ 0, -1]),
                         3:np.array([-1,  0])}
       self.orientation = 0
+      self.visited     = []
+
    def move(self, instruction):
       #get turn and distance
       rot   = instruction[0]
@@ -22,18 +24,35 @@ class compass:
       self.orientation %= 4
 
       #add multiple of unit array
+      for i in range(1, dist+1):
+         #using a list so we can use "in" finding repeats
+         #record positions between instructions
+         visit = list(self.position + i*self.direction[self.orientation])
+         self.visited.append(visit)
+
       self.position += dist*self.direction[self.orientation]
+
    def hamming(self):
       dist = sum(np.abs(self.position))
       return(dist)
+
+   def first_repeat(self):
+      seen = self.visited
+      for index, _ in enumerate(self.visited):
+         current  = self.visited[index]
+         previous = self.visited[:index]
+         if current in previous:
+            return(current)
 
 if __name__ == "__main__":
    #read input to integer list
    with open("../input.txt", 'r') as f:
       input_list = f.read().strip().split(', ')
 
+   #execute instructions
    nav  = compass()
    for instruction in input_list:
       nav.move(instruction)
       
    print(f"Part 1 answer: {nav.hamming()}")
+   print(f"Part 2 answer: {sum(nav.first_repeat())}")
