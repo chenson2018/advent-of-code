5224dd9fc65b941f243de84ba3bbea5b67c6c2af
Chris Henson <chrishenson.net@gmail.com>
2020-12-05 22:51:52 +0000

    Move summation to main

--- 2020/02/perl/02.pl
+++ 2020/02/perl/02.pl
@@ -1,48 +1,30 @@
 #!/usr/bin/env perl
 use experimental 'smartmatch';
+use List::Util qw/sum/;
 
 sub range_policy
    {
-   my $input_ref = shift;
-   my @lines = @{ $input_ref };
-   my $match = 0;
+   #regex to extract variables
+   my $pattern = '(\d+)\-(\d+) ([a-z]): ([a-z]+)'; 
+   my ($low, $high, $letter, $passwd) = $_ =~ /$pattern/;
+   my $count = () = $passwd =~ /\Q$letter/g;
 
-   foreach (@lines)
-      {
-      #regex to extract variables
-      my $pattern = '(\d+)\-(\d+) ([a-z]): ([a-z]+)'; 
-      my ($low, $high, $letter, $passwd) = $_ =~ /$pattern/;
-      my $count = () = $passwd =~ /\Q$letter/g;
-      
-      $match++ 
-         if ($count ~~ [$low..$high]);
-      }
-   return $match;
+   1 if ($count ~~ [$low..$high]);
 
    }
 
 sub position_policy
    {
-   my $input_ref = shift;
-   my @lines = @{ $input_ref };
-   my $match = 0;
+   #regex to extract variables
+   my $pattern = '(\d+)\-(\d+) ([a-z]): ([a-z]+)'; 
+   my ($low, $high, $letter, $passwd) = $_ =~ /$pattern/;
 
-   foreach (@lines)
-      {
-      #regex to extract variables
-      my $pattern = '(\d+)\-(\d+) ([a-z]): ([a-z]+)'; 
-      my ($low, $high, $letter, $passwd) = $_ =~ /$pattern/;
+   #adjusted for indexing at 1
+   my $index_1 = substr($passwd, $low  - 1, 1);
+   my $index_2 = substr($passwd, $high - 1, 1);
 
-      #adjusted for indexing at 1
-      my $index_1 = substr($passwd, $low  - 1, 1);
-      my $index_2 = substr($passwd, $high - 1, 1);
-
-      #exclusive or check
-      $match++ 
-         if (($letter eq $index_1)^($letter eq $index_2));
-      }
-   return $match;
-
+   #exclusive or check
+   1 if (($letter eq $index_1)^($letter eq $index_2));
    }
 
 
@@ -52,8 +34,8 @@ sub main
    chomp(my @input = <$handle>);
    close $handle;
 
-   my $p1 = range_policy(\@input);
-   my $p2 = position_policy(\@input);
+   my $p1 = sum( map{ range_policy   ($_) } @input );
+   my $p2 = sum( map{ position_policy($_) } @input );
 
    print "Part 1 answer: $p1" . "\n";
    print "Part 2 answer: $p2" . "\n";
