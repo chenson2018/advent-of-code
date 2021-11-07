64c52bec1d1818bece4dc5ff55d68febedaacebd
Chris Henson <chrishenson.net@gmail.com>
2021-11-07 18:05:26 -0500

    refactor printing into last let

--- 2015/01/ocaml/01.ml
+++ 2015/01/ocaml/01.ml
@@ -5,18 +5,22 @@
 
 open Core
 
-let p1 line = 
-  let action paren = 
+(* process a single '(' or ')' *)
+
+let interp_paren (paren: char) : int = 
     match paren with
       | '('->  1
       | ')'-> -1
-      | _ -> assert false in
-  let total = String.to_list line 
-                |> List.map ~f:action 
-                |> List.fold_left ~f:(+) ~init:0 in
-  printf "Position: %d \n" total
+      | _ -> assert false
+
+(* map a string to a list of 1/-1 *)
+
+let actions_list (line: string) : int list = 
+  String.to_list line 
+    |> List.map ~f:interp_paren
 
 let () =
-  let file_to_read = "../input.txt" in
-  let lines = In_channel.read_lines file_to_read in
-    List.iter ~f: p1 lines;
+  let input   = In_channel.read_all "../input.txt" in
+  let actions = actions_list input in
+  let p1_ans  = List.fold_left ~f:(+) ~init:0 actions in
+    printf "Part 1 answer: %d\n" p1_ans;
