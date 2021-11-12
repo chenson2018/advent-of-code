647ab11a90e97a239895e8096ced2a6a19f18de1
Chris Henson <chrishenson.net@gmail.com>
2021-11-12 11:01:38 -0500

    finish Day 6, 2015

--- 2015/06/ocaml/05.ml
+++ 2015/06/ocaml/05.ml
@@ -7,27 +7,12 @@
 open Core
 open Str
 
-(* type reresenting an individual light and its instruction *)
+(* variant reresenting an individual light and its instruction *)
 
 type light_action = 
   | Toggle | On | Off
 
-type light = 
-  {
-  x: int;
-  y: int;
-  action: light_action;
-  }
-
-(* calculate on/off given a state and a record with an action *)
-
-let calc_action state record = 
-  match record with
-  | {x = _; y = _; action = Toggle } -> not state
-  | {x = _; y = _; action = On     } -> true
-  | {x = _; y = _; action = Off    } -> false
-
-(* type representing an instruction for a given rectangle *)
+(* record representing an instruction for a given rectangle *)
 
 type instruction = 
   {
@@ -37,6 +22,20 @@ type instruction =
   x1: int;
   y1: int;
   }
+
+(* calculate on/off given a state and a record with an action *)
+
+let calc_action_p1 (state: bool) (record: instruction) : bool = 
+  match record with
+  | { x0=_; y0=_; x1=_; y1=_; action = Toggle } -> not state
+  | { x0=_; y0=_; x1=_; y1=_; action = On     } -> true
+  | { x0=_; y0=_; x1=_; y1=_; action = Off    } -> false
+
+let calc_action_p2 (state: int) (record: instruction) : int = 
+  match record with
+  | { x0=_; y0=_; x1=_; y1=_; action = Toggle } -> state + 2
+  | { x0=_; y0=_; x1=_; y1=_; action = On     } -> state + 1
+  | { x0=_; y0=_; x1=_; y1=_; action = Off    } -> max 0 (state - 1)
 
 (* convert a string into the light_action type *)
 
@@ -48,7 +47,7 @@ let action_of_string (s: string) : light_action =
   | _ -> assert false
 
 
-(* take a string and convert into a record *)
+(* take a string and convert into an instruction record *)
 
 let parse_instruction (s: string) : instruction = 
   let parse (n: int) (g: int): string = 
@@ -65,14 +64,45 @@ let parse_instruction (s: string) : instruction =
   
 
 (* 
-next steps:
-  - for each calculate the tuples affected and have a big list of light records
-  - for each possible tuple, filter the big list and fold with calc_action
+from https://stackoverflow.com/questions/10893521/how-to-take-product-of-two-list-in-ocaml
+I think this is not tail recursive
+*)
+
+let cartesian l l' = 
+  List.concat (List.map ~f:(fun e -> List.map ~f:(fun e' -> (e,e')) l') l)
+
+(* from https://stackoverflow.com/questions/243864/what-is-the-ocaml-idiom-equivalent-to-pythons-range-function *)
+
+let (--) i j = 
+    let rec aux n acc =
+      if n < i then acc else aux (n-1) (n :: acc)
+    in aux j []
+
+(* this returns a boolean indicating if an instruction affects a particuliar record *)
+
+let ins_includes (tup: (int * int) ) (ins: instruction): bool = 
+  match tup with
+  | (x, y) -> (ins.x0 <= x) && (x <= ins.x1) && (ins.y0 <= y) && (y <= ins.y1)
+
+
+(*
+The progrssion is
+
+list of instruction records ->
+map each grid point to a filtered list of instructions that affect it, type instruction list list ->
+for each problem, map this lists of lists to a fold to get the light state, so int list ->
+get the final summation
+
 *)
 
 let () =
   let input = In_channel.read_lines "../input.txt" in
+  let grid = cartesian (0--999) (0--999) in
   let instructions = List.map ~f:parse_instruction input in
-  let test = List.find_exn ~f:(fun ins -> match ins with | {action = Toggle} -> true | {action = _ } -> false) instructions in
-    printf "%d\n" (List.length instructions);
-    printf "%d\n" test.x1;
+  let light_l = List.map ~f:(fun g -> List.filter ~f:(fun ins -> ins_includes g ins ) instructions) grid in 
+  let states_p1 = List.map ~f:(fun l -> Bool.to_int (List.fold_left ~f:calc_action_p1 ~init:false l)) light_l in
+  let states_p2 = List.map ~f:(fun l -> List.fold_left ~f:calc_action_p2 ~init:0 l) light_l in
+  let p1_ans  = List.fold_left ~f:(+) ~init:0 states_p1 in
+  let p2_ans  = List.fold_left ~f:(+) ~init:0 states_p2 in
+    printf "Part 1 answer: %d\n" p1_ans;
+    printf "Part 2 answer: %d\n" p2_ans;
