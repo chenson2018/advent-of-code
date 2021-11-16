7b204b42babd65ccd9d1d2a7df5814f43f37f259
Chris Henson <chrishenson.net@gmail.com>
2021-11-16 15:34:27 -0500

    types rework

--- 2015/07/ocaml/07.ml
+++ 2015/07/ocaml/07.ml
@@ -8,114 +8,119 @@ open Core
 open Str
 open Hashtbl
 
-(* 
-this type represents a singal
-can be either an integer or a variable
-raises an error for invalid integer signal
-*)
+(* a variable or integer *)
 
 type signal = 
   | Var of string
   | Val of int
 
-type expr = 
-  | NOT of signal
-  | AND of signal * signal
-  | OR of signal * signal
-  | LSHIFT of signal * signal
-  | RSHIFT of signal * signal
-  | SIGNAL of signal
+(* an operator on the lhs *)
 
-let signal_of_string s = 
+type operator = 
+  {
+  op_lhs: signal option;
+  op: string;
+  op_rhs: signal;
+  } 
+
+(* an lhs can be a signal or operator *)
+
+type lhs = 
+  | Signal of signal
+  | Operator of operator
+
+(* simpliy an operator where all signals are know Val int *)
+
+let simplify_op (op: lhs): lhs = 
+  match op with
+  | Operator {op_lhs = Some (Val l); op = "AND"    ; op_rhs = Val r} -> Signal (Val (l land  r))
+  | Operator {op_lhs = Some (Val l); op = "OR"     ; op_rhs = Val r} -> Signal (Val (l lor   r))
+  | Operator {op_lhs = Some (Val l); op = "LSHIFT" ; op_rhs = Val r} -> Signal (Val (l lsl   r))
+  | Operator {op_lhs = Some (Val l); op = "RSHIFT" ; op_rhs = Val r} -> Signal (Val (l lsr   r))
+  | Operator {op_lhs =         None; op = "NOT"    ; op_rhs = Val r} -> Signal (Val (65535 - r))
+  | _ -> op 
+
+let print_lhs (l: lhs): unit = 
+  match l with
+  | Signal (Val x) -> printf "Val: %d\n" x
+  | Signal (Var x) -> printf "Var: %s\n" x
+  | Operator {op_lhs=_; op=o; op_rhs=_} -> printf "Operator %s called\n" o
+
+(* assignment of an operator to a signal *)
+
+type assignment = 
+  {
+  l: lhs;
+  r: signal; (* should really only be Var of string, but not sure how *)
+  }
+
+(* parse lhs *)
+let signal_of_string (s: string): signal =
   try
     Val (int_of_string s)
   with
     Failure _ -> Var s
-(*
-when doing two regex the match is reset!!!
 
-  if ( string_match ( regexp "^[a-z]+$" ) s 0 ) then
-    Var s
-  else
-  if ( string_match ( regexp "^[0-9]+$" ) s 0 ) then
-    Val (int_of_string s)
-  else
-    assert false
-*)
-  
-let string_of_signal (si: signal) : string = 
-  match si with 
-  | Var x -> x
-  | Val x -> string_of_int x
-
-let expr_of_string (s: string) : expr = 
+let lhs_of_string (s: string) : lhs = 
   if ( string_match ( regexp "^[a-z0-9]+$" ) s 0 ) then
-    SIGNAL (signal_of_string s) else 
+    Signal (signal_of_string s) else
 
   if ( string_match ( regexp "^NOT \\([a-z0-9]+\\)$" ) s 0 ) then
-    NOT (signal_of_string ( Str.matched_group 1 s ) ) else
+    Operator {op_lhs = None; 
+              op = "NOT"; 
+              op_rhs = (signal_of_string (Str.matched_group 1 s));} 
+  else
 
   if ( string_match ( regexp "^\\([a-z0-9]+\\) AND \\([a-z0-9]+\\)$" ) s 0 ) then
-    AND (signal_of_string (Str.matched_group 1 s),
-         signal_of_string (Str.matched_group 2 s)) else
+    Operator {op_lhs = Some ( signal_of_string (Str.matched_group 1 s) );
+              op = "AND"; 
+              op_rhs = (signal_of_string (Str.matched_group 2 s));} 
+  else
 
   if ( string_match ( regexp "^\\([a-z0-9]+\\) OR \\([a-z0-9]+\\)$" ) s 0 ) then
-    OR (signal_of_string (Str.matched_group 1 s),
-        signal_of_string (Str.matched_group 2 s)) else
+    Operator {op_lhs = Some ( signal_of_string (Str.matched_group 1 s) );
+              op = "OR"; 
+              op_rhs = (signal_of_string (Str.matched_group 2 s));} 
+  else
 
   if ( string_match ( regexp "^\\([a-z0-9]+\\) LSHIFT \\([a-z0-9]+\\)$" ) s 0 ) then
-    LSHIFT (signal_of_string (Str.matched_group 1 s),
-            signal_of_string (Str.matched_group 2 s)) else
+    Operator {op_lhs = Some ( signal_of_string (Str.matched_group 1 s) );
+              op = "LSHIFT"; 
+              op_rhs = (signal_of_string (Str.matched_group 2 s));} 
+  else
 
   if ( string_match ( regexp "^\\([a-z0-9]+\\) RSHIFT \\([a-z0-9]+\\)$" ) s 0 ) then
-    RSHIFT (signal_of_string (Str.matched_group 1 s), 
-            signal_of_string (Str.matched_group 2 s))
-
+    Operator {op_lhs = Some ( signal_of_string (Str.matched_group 1 s) );
+              op = "RSHIFT"; 
+              op_rhs = (signal_of_string (Str.matched_group 2 s));} 
   else
     assert false
 
-let print_expr (e: expr) : unit = 
-  match e with
-  | SIGNAL s -> printf "SIGNAL: %s\n" (string_of_signal s)
-  | NOT s -> printf "NOT %s\n" (string_of_signal s)
-  | AND (s, s') -> printf "%s AND %s\n" (string_of_signal s) (string_of_signal s')
-  | OR (s, s') -> printf "%s OR %s\n" (string_of_signal s) (string_of_signal s')
-  | LSHIFT (s, s') -> printf "%s LSHIFT %s\n" (string_of_signal s) (string_of_signal s')
-  | RSHIFT (s, s') -> printf "%s RSHIFT %s\n" (string_of_signal s) (string_of_signal s')
-
-(* a record representing an assignment *)
-
-type assignment = 
-  {
-  mutable lhs: expr;
-  rhs: expr;
-  }
+(* read input *)
 
 let parse_assignment (s: string) : assignment = 
-  let parse (n: int) (g: int): string = 
-    Str.matched_group g s 
+  let parse (n: int) (g: int): string =
+    Str.matched_group g s
   in
-  (* I'm using the OCaml regex for now, put it's pretty awful compared to pcre *)
   let pattern = regexp "\\(.*?\\) -> \\(.*\\)" in
   let n = Str.search_forward pattern s 0 in
 
   let lhs_s = parse n 1 in
   let rhs_s = parse n 2 in
 
-  {lhs = expr_of_string lhs_s; 
-   rhs = expr_of_string rhs_s;}
+  {l = lhs_of_string lhs_s;
+   r = Var rhs_s;}
 
 (* iterate through assignments an see which are fully resolved *)
 
-let hash_val ht a = 
+let hash_val ht a =
   match a with
-  | {lhs = SIGNAL (Val value); rhs = SIGNAL (Var key) } -> Hashtbl.set ~key:key ~data:value ht  
+  | {l = Signal (Val value); r =  Var key } -> Hashtbl.set ~key:key ~data:value ht
   | _ -> ()
 
-let () =
+let () = 
   let input = In_channel.read_lines "../input.txt" in
   let assignments = List.map ~f:parse_assignment input in
   let ht = Hashtbl.create (module String) in
     List.iter ~f:(fun a -> hash_val ht a) assignments;
     Hashtbl.iteri ht ~f:(fun ~key ~data -> print_endline (Printf.sprintf "%s->%d" key data));
-(*    List.iter ~f:(fun a -> print_expr a.lhs) assignments; *)
