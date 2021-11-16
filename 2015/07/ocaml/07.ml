1079f3fd345a5319026fdbd0433a8cfd5cdee93e
Chris Henson <chrishenson.net@gmail.com>
2021-11-16 01:02:39 -0500

    somewhat working types... still very messy

--- 2015/07/ocaml/07.ml
+++ 2015/07/ocaml/07.ml
@@ -18,30 +18,75 @@ type signal =
   | Val of int
 
 type expr = 
+  | NOT of signal
+  | AND of signal * signal
+  | OR of signal * signal
+  | LSHIFT of signal * signal
+  | RSHIFT of signal * signal
   | SIGNAL of signal
 
-(* I was going to just use a regex, but had some issues... will come back to fix *)
+let signal_of_string s = 
+  try
+    Val (int_of_string s)
+  with
+    Failure _ -> Var s
+(*
+when doing two regex the match is reset!!!
 
-let expr_of_string (s: string) : expr =
   if ( string_match ( regexp "^[a-z]+$" ) s 0 ) then
-    SIGNAL (Var s)
+    Var s
   else
   if ( string_match ( regexp "^[0-9]+$" ) s 0 ) then
-    SIGNAL (Val (int_of_string s))
-  else 
-    SIGNAL (Var "Not Implemented")
+    Val (int_of_string s)
+  else
+    assert false
+*)
+  
+let string_of_signal (si: signal) : string = 
+  match si with 
+  | Var x -> x
+  | Val x -> string_of_int x
+
+let expr_of_string (s: string) : expr = 
+  if ( string_match ( regexp "^[a-z0-9]+$" ) s 0 ) then
+    SIGNAL (signal_of_string s) else 
+
+  if ( string_match ( regexp "^NOT \\([a-z0-9]+\\)$" ) s 0 ) then
+    NOT (signal_of_string ( Str.matched_group 1 s ) ) else
+
+  if ( string_match ( regexp "^\\([a-z0-9]+\\) AND \\([a-z0-9]+\\)$" ) s 0 ) then
+    AND (signal_of_string (Str.matched_group 1 s),
+         signal_of_string (Str.matched_group 2 s)) else
+
+  if ( string_match ( regexp "^\\([a-z0-9]+\\) OR \\([a-z0-9]+\\)$" ) s 0 ) then
+    OR (signal_of_string (Str.matched_group 1 s),
+        signal_of_string (Str.matched_group 2 s)) else
+
+  if ( string_match ( regexp "^\\([a-z0-9]+\\) LSHIFT \\([a-z0-9]+\\)$" ) s 0 ) then
+    LSHIFT (signal_of_string (Str.matched_group 1 s),
+            signal_of_string (Str.matched_group 2 s)) else
+
+  if ( string_match ( regexp "^\\([a-z0-9]+\\) RSHIFT \\([a-z0-9]+\\)$" ) s 0 ) then
+    RSHIFT (signal_of_string (Str.matched_group 1 s), 
+            signal_of_string (Str.matched_group 2 s))
+
+  else
+    assert false
 
 let print_expr (e: expr) : unit = 
-  match e with 
-  | SIGNAL (Var x) -> printf "VAR: %s\n" x
-  | SIGNAL (Val x) -> printf "VAL: %d\n" x
-  | _ -> printf "%s" "Not implemented"
+  match e with
+  | SIGNAL s -> printf "SIGNAL: %s\n" (string_of_signal s)
+  | NOT s -> printf "NOT %s\n" (string_of_signal s)
+  | AND (s, s') -> printf "%s AND %s\n" (string_of_signal s) (string_of_signal s')
+  | OR (s, s') -> printf "%s OR %s\n" (string_of_signal s) (string_of_signal s')
+  | LSHIFT (s, s') -> printf "%s LSHIFT %s\n" (string_of_signal s) (string_of_signal s')
+  | RSHIFT (s, s') -> printf "%s RSHIFT %s\n" (string_of_signal s) (string_of_signal s')
 
 (* a record representing an assignment *)
 
 type assignment = 
   {
-  lhs: string;
+  lhs: expr;
   rhs: expr;
   }
 
@@ -56,10 +101,11 @@ let parse_assignment (s: string) : assignment =
   let lhs_s = parse n 1 in
   let rhs_s = parse n 2 in
 
-  {lhs = lhs_s; 
+  {lhs = expr_of_string lhs_s; 
    rhs = expr_of_string rhs_s;}
 
 let () =
   let input = In_channel.read_lines "../input.txt" in
   let assignments = List.map ~f:parse_assignment input in
-    List.iter ~f:(fun a -> print_expr a.rhs) assignments;
+  let l1_assignments = List.filter ~f:(fun a -> match a with | {lhs = SIGNAL (Val _); rhs = _ } -> true | _ -> false) assignments in
+    List.iter ~f:(fun a -> print_expr a.lhs) l1_assignments;
