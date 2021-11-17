1fe19ac034f41974399ba67c3c20a1c82d59014e
Chris Henson <chrishenson.net@gmail.com>
2021-11-17 00:24:33 -0500

    Day 7, 2015 (part 2)

--- 2015/07/ocaml/07.ml
+++ 2015/07/ocaml/07.ml
@@ -9,13 +9,11 @@ open Str
 open Hashtbl
 
 (* a variable or integer *)
-
 type signal = 
   | Var of string
   | Val of int
 
 (* an operator on the lhs *)
-
 type operator = 
   {
   op_lhs: signal option;
@@ -24,13 +22,11 @@ type operator =
   } 
 
 (* a lhs can be a signal or operator *)
-
 type lhs = 
   | Signal of signal
   | Operator of operator
 
 (* simplify an operator where all signals are known Val int *)
-
 let simplify_op (op: lhs): lhs = 
   match op with
   | Operator {op_lhs = Some (Val l); op = "AND"    ; op_rhs = Val r} -> Signal (Val (l land  r))
@@ -40,17 +36,18 @@ let simplify_op (op: lhs): lhs =
   | Operator {op_lhs =         None; op = "NOT"    ; op_rhs = Val r} -> Signal (Val (65535 - r))
   | _ -> op 
 
-(* assignment of an operator to a signal *)
-
+(* 
+  assignment of an operator to a signal 
+  r should only be a Var String, but I had trouble with types,
+  but note that this is enfored in the parser function however 
+*)
 type assignment = 
   {
   l: lhs;
-  r: signal; (* should onlybe a Var String, but I had trouble getting that here
-                note that this is enfored in the parser function however *)
+  r: signal;
   }
 
 (* I'd prefer to use regex but having trouble with matches being reset *)
-
 let signal_of_string (s: string): signal =
   try
     Val (int_of_string s)
@@ -58,7 +55,6 @@ let signal_of_string (s: string): signal =
     Failure _ -> Var s
 
 (* parse lhs *)
-
 let lhs_of_string (s: string) : lhs = 
   if ( string_match ( regexp "^[a-z0-9]+$" ) s 0 ) then
     Signal (signal_of_string s) else
@@ -95,7 +91,6 @@ let lhs_of_string (s: string) : lhs =
     assert false
 
 (* parse full input lines *)
-
 let parse_assignment (s: string) : assignment = 
   let parse (n: int) (g: int): string =
     Str.matched_group g s
@@ -117,11 +112,10 @@ let hash_val (ht: (string, int) t) (a: assignment) : unit =
 
 
 (* 
-one iteration of simplification this takes an assignment then
+  one iteration of simplification this takes an assignment then
 
- 1) replaces values already in the hash
- 2) then simplifies lhs if possible
-
+  1) replaces values already in the hash
+  2) then simplifies lhs if possible
 *)
 let replace_hash (ht: (string, int) t ) (a: assignment) : assignment = 
   match a.l with
@@ -150,10 +144,30 @@ let rec find_wire (ht: (string, int) t ) (al: assignment list) (find: string): i
 
 let () = 
   let input = In_channel.read_lines "../input.txt" in 
-  let assignments = List.map ~f:parse_assignment input in
-  let ht = Hashtbl.create (module String) in
-  let wire_a = find_wire ht assignments "a" in
-    printf "Part 1 answer: %d\n" wire_a;
-    (* nice way to print the hash *)
-    (* Hashtbl.iteri ht ~f:(fun ~key ~data -> print_endline (Printf.sprintf "%s->%d" key data)); *)
+  let target_wire = "a" in
+  (* 
+    parse the assignments
+    create a hash for storing resolved wires
+    simplify until the has contains the desired key 
+  *)
+  let assignments_p1 = List.map ~f:parse_assignment input in
+  let ht_p1 = Hashtbl.create (module String) in
+  let p1_ans = find_wire ht_p1 assignments_p1 target_wire in
+    printf "Part 1 answer: %d\n" p1_ans;
+
+  (* now for part 2, same problem but change the value of a wire *)
+  (* hung when I tried to use a variable for "b" in a pattern match, should inverstigate later *)
+  let ht_p2 = Hashtbl.create (module String) in
+  let assignments_p2 =  ({l = Signal (Val p1_ans); r = Var "b"})
+                      ::(List.filter ~f:(fun a -> 
+                                          match a.r with 
+                                          | Var "b" -> false 
+                                          | _       -> true )
+                         assignments_p1) 
+  in
+  let p2_ans = find_wire ht_p2 assignments_p2 target_wire in
+    printf "Part 2 answer: %d\n" p2_ans;
+
+(* nice way to print hashes *)
+(* Hashtbl.iteri ht ~f:(fun ~key ~data -> print_endline (Printf.sprintf "%s->%d" key data)); *)
 
