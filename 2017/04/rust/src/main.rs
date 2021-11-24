725a63a32426c637e569b8908b349cea90d1fc14
Chris Henson <chrishenson.net@gmail.com>
2021-11-24 15:28:16 -0500

    2017 Day 4 (part 2)

--- 2017/04/rust/src/main.rs
+++ 2017/04/rust/src/main.rs
@@ -1,23 +1,35 @@
 use std::fs;
 use itertools::Itertools;
 
-fn main() {
-  let input_s = fs::read_to_string("../input.txt").expect("Unable to read file");
-  
-  let input_vv: Vec<Vec<String>> = input_s.lines()
-      .map(|s| s.split_whitespace()
-                .filter_map(|word| word.parse().ok() )
+fn count_unique(input_s: &str, problem: &str) -> usize {
+  let input: Vec<Vec<String>> = input_s.lines()
+      .map(|s: &str| s.split_whitespace()
+                .map(|s: &str|
+                  match problem {
+                    "p1" => s.chars()         .collect(),
+                    "p2" => s.chars().sorted().collect(),
+                     _   => panic!("Unexpected argument: {:?}", problem),
+                 })
                 .collect())
       .collect();
 
-  let p1_ans = input_vv.iter()
-                 .map(|v| {
-                   ( v.len() == 
-                     v.into_iter().unique().collect::<Vec<&String>>().len() 
-                   ) as usize 
-                  })
-                 .collect::<Vec<usize>>()
-                 .iter().sum::<usize>();
+  input
+    .iter()
+    .map(|v| {
+      ( v.len() == 
+        v.into_iter().unique().collect::<Vec<&String>>().len() 
+      ) as usize 
+     })
+    .collect::<Vec<usize>>()
+    .iter().sum::<usize>()
+}
 
+fn main() {
+  let input_s = fs::read_to_string("../input.txt").expect("Unable to read file");
+
+  let p1_ans = count_unique(&input_s, "p1");
   println!("Part 1 answer: {:?}", p1_ans);
+
+  let p2_ans = count_unique(&input_s, "p2");
+  println!("Part 2 answer: {:?}", p2_ans);
 }
