2a4f0bb35b5292c830723078b1411e14cb9892e0
Chris Henson <chrishenson.net@gmail.com>
2021-11-24 20:01:18 -0500

    2017 Day 7 (part 1 in shell)

---
+++ 2017/07/shell/07.sh
@@ -0,0 +1,8 @@
+# a one-liner solution to part one
+# first grep gets the entries on the lhs,
+# second geths the rhs
+# then comm finds the difference
+
+comm -23                                            \
+  <(cat <<<$(grep -oP "^\w+"   ../input.txt | sort)) \
+  <(cat <<<$(grep -oP " \K\w+" ../input.txt | sort))  \
