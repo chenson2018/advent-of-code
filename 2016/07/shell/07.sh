42595c29fec994f7c20ebb5f50534c8d24665be8
Chris Henson <chrishenson.net@gmail.com>
2021-11-29 01:58:22 -0500

    Day 7 2016 (part 2)

--- 2016/07/shell/07.sh
+++ 2016/07/shell/07.sh
@@ -1,9 +1,34 @@
-# more regex tomfoolery
-# first, filter out entries that have an abba pattern inside []
+# more regex tomfoolery and generally just me pushing how much I can do in just the command line
+# YMMV if I missed an edge case not in my input
+
+# Part 1
+# filter out entries that have an abba pattern inside [],
 # then find remaining with abba pattern
 
-grep -vP '\[[^]]*(.)(.)(?!\1)\2\1' ../input.txt | grep -P '(.)(.)(?!\1)\2\1' | wc -l | xargs echo "Part 1 answer:"
+grep -vP '\[[^]]*(.)(.)(?!\1)\2\1' ../input.txt |
+grep  -P        '(.)(.)(?!\1)\2\1'              |
+wc -l                                           |
+xargs echo "Part 1 answer:"
 
-# WIP, on part 2, not quite right
-# grep -P '\[[^]]*(.)(.)\1.*?\].*?\2\1\2' ../input.txt | wc -l
-# grep -P '(.)(.)\1.*?\[[^]]*\2\1\2' ../input.txt | wc -l
+# Part 2
+# this is extremely inefficient in how it passes around the whole file as a string variable
+# I move all the [] blocks to the end of the string so they are easier to match,
+# leaving a pipe in their place so that I don't add extra matches
+
+move_to_end='match($0,/\[[^]]*\]/){
+  print substr($0,1,RSTART-1)"|"substr($0,RSTART+RLENGTH)substr($0,RSTART,RLENGTH)
+}' 
+
+# max number of [.*]  that appear in a line
+max_bracket=$(sed 's/[^[]//g' ../input.txt | awk '{ print length }' | sort | tail -n1)
+
+input=$(cat ../input.txt)
+
+for ((i=1;i<=$max_bracket;i++)); do
+  input=$(echo "$input" | awk "$move_to_end")
+done
+
+# now that we have [] all at the end, our capture groups are greatly simplified
+grep -P '^[^[]*(.)(.)\1.*?\[.*?\2\1\2' <<< $input | 
+wc -l                                             | 
+xargs echo "Part 2 answer:"
