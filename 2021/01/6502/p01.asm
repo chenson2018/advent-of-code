5fe7ecec65e8da5b276d45a558d8f9f891360c0b
Chris Henson <chrishenson.net@gmail.com>
2021-12-02 15:47:46 -0500

    working subtraction

---
+++ 2021/01/6502/p01.asm
@@ -0,0 +1,21 @@
+ADR2_LOW  = $6100
+ADR2_HIGH = $6101
+
+ADR1_LOW  = $6102
+ADR1_HIGH = $6103
+
+RES_LOW   = $6200
+RES_HIGH  = $6201 
+
+SUB16: SEC      ; CLEAR CARRY BIT
+       CLD      ; CLEAR DECIMAL BIT
+
+       LDA ADR1_LOW
+       SBC ADR2_LOW
+       STA RES_LOW
+
+       LDA ADR1_HIGH
+       SBC ADR2_HIGH
+       STA RES_HIGH
+
+       RTS
