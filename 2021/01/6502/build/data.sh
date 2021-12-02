a87b621199bb8ce75fc3e95cc67bbe4012e18818
Chris Henson <chrishenson.net@gmail.com>
2021-12-02 13:37:23 -0500

    working with the test data, not working for actual input

--- 2021/01/6502/build/data.sh
+++ 2021/01/6502/build/data.sh
@@ -1,7 +1,10 @@
 # This is used to generate some assembly to load our data
 
-awk '{printf "%04x\n", $1}' ../input.txt | 
-#awk '{printf "%04x\n", $1}' test.txt | 
+# this is working with the test data, but then fails with the larger input....
+# I have no idea why yet
+
+#awk '{printf "%04x\n", $1}' ../input.txt | 
+awk '{printf "%04x\n", $1}' test.txt | 
 gawk -v DATA_ADR=24832 'match($0, /(..)(..)/, h)  { 
   printf "LDA #$%s\n",  h[1];
   printf "STA $%04x\n", DATA_ADR;
