0ffdb5dc510cc7fb55c6a8bbdc77cc215e3470b3
Chris Henson <chrishenson.net@gmail.com>
2021-12-07 01:57:07 -0500

    2021 Day 6 (R)

---
+++ 2021/06/R/06.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+options(scipen=999)
+
+# get the initial counts for each position
+input <- unlist(read.table("../input.txt", sep = ","))
+init  <- sapply(0:8, function(num) sum(input == num))
+
+# this is a transition matrix for how fish spawn
+# this matrix is transposed to make it easier to read
+
+transition <- matrix(c(0, 0, 0, 0, 0, 0, 1, 0, 1, # 0 -> 6, 8
+                       1, 0, 0, 0, 0, 0, 0, 0, 0, # 1 -> 0
+                       0, 1, 0, 0, 0, 0, 0, 0, 0, # 2 -> 1
+                       0, 0, 1, 0, 0, 0, 0, 0, 0, # etc.
+                       0, 0, 0, 1, 0, 0, 0, 0, 0, 
+                       0, 0, 0, 0, 1, 0, 0, 0, 0, 
+                       0, 0, 0, 0, 0, 1, 0, 0, 0, 
+                       0, 0, 0, 0, 0, 0, 1, 0, 0, 
+                       0, 0, 0, 0, 0, 0, 0, 1, 0), 
+                     ncol = 9)
+
+# advance the fish by one iteration
+# I add an unused argument so that I can pass to Reduce for multiple iterations
+
+iterate <- function(state, ign) {
+  transition %*% state
+}
+
+cat("Part 1 answer:", sum(Reduce(iterate, 1:80 , init = init)), "\n")
+cat("Part 2 answer:", sum(Reduce(iterate, 1:256, init = init)), "\n")
