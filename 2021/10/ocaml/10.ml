47f9fcdd59a99240e723165909c89b35e85ae281
Chris Henson <chrishenson.net@gmail.com>
2021-12-11 22:26:05 -0500

    cleaning

--- 2021/10/ocaml/10.ml
+++ 2021/10/ocaml/10.ml
@@ -6,27 +6,27 @@
 open Core
 open Hashtbl
 
-type closing = 
+type paired_char = 
   {
   c: char;
   score: int;
   }
 
-let scores_hash  = Hashtbl.of_alist_exn (module Char) [
-                                                        (')', {c = '('; score = 3    });
-                                                        (']', {c = '['; score = 57   });
-                                                        ('}', {c = '{'; score = 1197 });
-                                                        ('>', {c = '<'; score = 25137})
-                                                      ];;
+let error_score_hash       = Hashtbl.of_alist_exn (module Char)
+                             [
+                               (')', {c = '('; score = 3    });
+                               (']', {c = '['; score = 57   });
+                               ('}', {c = '{'; score = 1197 });
+                               ('>', {c = '<'; score = 25137})
+                             ];;
 
-let scores_hash2  = Hashtbl.of_alist_exn (module Char) [
-                                                        ('(', {c = ')'; score = 1});
-                                                        ('[', {c = ']'; score = 2});
-                                                        ('{', {c = '}'; score = 3});
-                                                        ('<', {c = '>'; score = 4})
-                                                      ];;
-
-
+let incomplete_score_hash  = Hashtbl.of_alist_exn (module Char)
+                             [
+                               ('(', {c = ')'; score = 1});
+                               ('[', {c = ']'; score = 2});
+                               ('{', {c = '}'; score = 3});
+                               ('<', {c = '>'; score = 4})
+                             ];;
 (* 
 
 I successively take each character and move it to another list, eliminating matching paris as needed
@@ -43,31 +43,32 @@ Small example: "[{<()}"
 *)
 
 let parse_line (xs: char list) : (char list * char list) = 
-  let rec aux left right = 
+  let rec aux (left: char list) (right: char list) : (char list * char list) = 
     match left, right with
-    | current :: tl_l, next :: tl_r -> (match Hashtbl.find scores_hash next with
-                                        | None -> aux (next::left) tl_r
-                                        | Some req_open -> if (Char.equal current req_open.c) 
+    | current :: tl_l, next :: tl_r -> (match Hashtbl.find error_score_hash next with
+                                        | None -> aux (next::left) tl_r                        (* if not a closing bracket, push to left and continue     *)
+                                        | Some req_open -> if (Char.equal current req_open.c)  (* otherwise, check that it si the correct closing bracket *)
                                                            then aux tl_l tl_r
                                                            else (left, right))
     | _ -> (left, right)
   in
+  (* here we make the initial character to the left list *)
   match xs with
   | hd :: tl -> aux [hd] tl
   | _        -> assert false
 
-let score (xs: char list): int = 
+let error_score (xs: char list): int = 
   match xs with
   | []     -> 0
-  | err::_ -> (match Hashtbl.find scores_hash err with
+  | err::_ -> (match Hashtbl.find error_score_hash err with
                | Some x -> x.score
                | None   -> assert false)
 
-let score2 xs = 
-  let advance cur c = 
-    match Hashtbl.find scores_hash2 c with
-    | None -> assert false
-    | Some x -> 5*cur + x.score
+let incomplete_score (xs: char list): int = 
+  let advance (cum: int) (c: char) : int = 
+    match Hashtbl.find incomplete_score_hash c with
+    | None   -> assert false
+    | Some x -> 5*cum + x.score
   in
   List.fold_left ~f:advance ~init:0 xs
 
@@ -75,23 +76,28 @@ let () =
   let input  = In_channel.read_lines "../input.txt" |> List.map ~f:String.to_list in
   
   let end_state = input |> List.map ~f:parse_line in
-
-
-  let p1_ans = end_state |> 
-                 List.map ~f:(fun state -> score (snd state)) |>  
+  let error_lists = List.map ~f:snd end_state in
+  let p1_ans = error_lists |> 
+                 List.map ~f:error_score |>  
                  List.fold_left ~f:(+) ~init:0 in
 
      printf "Part 1 answer: %d\n" p1_ans;
 
 
-  let incomplete = List.filter ~f:(fun state -> (score (snd state)) = 0) end_state in
-
-  let p2_score = incomplete |> List.map ~f:(fun x -> score2 (fst x)) in
-
-  let p2_ans = List.nth_exn ( p2_score |> List.sort ~compare:Int.compare) (List.length p2_score / 2) in
+  let incomplete = end_state |>
+                   List.filter ~f:(fun state -> match state with 
+                                                | (_,[]) -> true              (* if there are no errors, second tuple entry will be empty   *) 
+                                                | _      -> false)            (* could also just use the error_score function               *)
+  in 
+  let remaining_symbols  = List.map ~f:fst incomplete in                      (* grab the first tuple element, which are left to be matched *)
+  let p2_ans = List.nth_exn 
+               ( remaining_symbols |> 
+                   List.map ~f:incomplete_score |> 
+                   List.sort ~compare:Int.compare
+               ) 
+               (* median index for an odd length list *)
+               (List.length remaining_symbols / 2) 
+  in 
 
     printf "Part 2 answer: %d\n" p2_ans;
 
-(*  let p1_ans = input |> List.map ~f:score_corrupt |> List.fold_left ~f:(+) ~init:0 in
-    printf "Part 1 answer: %d\n" p1_ans;
-*)
