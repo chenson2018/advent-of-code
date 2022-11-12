efde25e1cc4d8fdb60603ac4a674c7da089ca7f4
Chris Henson <chrishenson.net@gmail.com>
2022-11-12 14:02:16 -0500

    thinking about part 2

--- 2021/21/rust/src/main.rs
+++ 2021/21/rust/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::cmp::min;
 
 #[derive(Debug)]
@@ -47,19 +48,25 @@ impl Game {
     fn playing(&self) -> bool {
         (self.p1.score < 1000) && (self.p2.score < 1000)
     }
+
+    fn game_deterministic(&mut self) {
+        let mut deterministic = (1..=100).cycle();
+
+        while self.playing() {
+            let roll: u32 = deterministic.by_ref().take(3).sum();
+            self.turn(roll);
+        }
+    }
+
+    fn metric(&self) -> u32 {
+        self.rolls * min(self.p1.score, self.p2.score)
+    }
 }
 
 fn main() {
     let mut game = Game::new(4, 1);
-    let mut deterministic = (1..=100).cycle();
-
-    while game.playing() {
-        let roll: u32 = deterministic.by_ref().take(3).sum();
-        game.turn(roll);
-    }
-
-    println!(
-        "Part 1 answer: {:?}",
-        game.rolls * min(game.p1.score, game.p2.score)
-    );
+    game.game_deterministic();
+    println!("Part 1 answer: {:?}", game.metric());
+    //let combo: Vec<_> = (1..=2).map(|_| 3..=9).multi_cartesian_product().collect();
+    //println!("{:?}", combo);
 }
