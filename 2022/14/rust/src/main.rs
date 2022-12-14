13a87e4e45a4c24184c70b938f7b429eea55a635
chenson2018 <46805207+chenson2018@users.noreply.github.com>
2022-12-14 03:42:46 -0500

    2022 Day 14

---
+++ 2022/14/rust/src/main.rs
@@ -0,0 +1,150 @@
+use std::cmp::{max, min};
+use std::collections::HashSet;
+
+enum Part {
+    Part1,
+    Part2,
+}
+
+#[derive(Eq, Hash, PartialEq, Debug, Clone, Copy)]
+struct Filled {
+    x: usize,
+    y: usize,
+}
+
+fn parse_rocks(input: &str) -> HashSet<Filled> {
+    let mut filled: HashSet<Filled> = HashSet::new();
+
+    // this is a little messy, but good enough for AOC
+    for line in input.lines() {
+        let corners: Vec<(usize, usize)> = line
+            .split(" -> ")
+            .map(|l| {
+                let (x, y) = if let Some((x, y)) = l.split_once(",") {
+                    (x, y)
+                } else {
+                    panic!();
+                };
+                (x.parse().unwrap(), y.parse().unwrap())
+            })
+            .collect();
+
+        let pairs = corners.windows(2);
+
+        for pair in pairs {
+            match pair {
+                [(x1, y1), (x2, y2)] => {
+                    let low_x = min(x1, x2);
+                    let upp_x = max(x1, x2);
+
+                    let low_y = min(y1, y2);
+                    let upp_y = max(y1, y2);
+
+                    for x in *low_x..=*upp_x {
+                        for y in *low_y..=*upp_y {
+                            filled.insert(Filled { x, y });
+                        }
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+    return filled;
+}
+
+impl Filled {
+    fn priority(&self) -> [Filled; 3] {
+        [
+            Filled {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Filled {
+                x: self.x - 1,
+                y: self.y + 1,
+            },
+            Filled {
+                x: self.x + 1,
+                y: self.y + 1,
+            },
+        ]
+    }
+
+    fn tick(&self, fill: &mut HashSet<Filled>, limit: usize, part: &Part) -> bool {
+        let mut pos = self.clone();
+
+        loop {
+            // record starting position
+            let start = pos;
+
+            // check in order of priority which open spot is empty
+            for m in pos.priority() {
+                if !fill.contains(&m) {
+                    pos = m;
+                    break;
+                }
+            }
+
+            // check if we weren't able to move
+            if start == pos {
+                break;
+            }
+
+            match part {
+                Part::Part1 => {
+                    // stop for infinite falling
+                    if pos.y >= limit {
+                        break;
+                    }
+                }
+                Part::Part2 => {
+                    // stop for floor
+                    if pos.y == limit + 1 {
+                        break;
+                    }
+                }
+            }
+        }
+
+        match part {
+            Part::Part1 => {
+                // fill until something fell past the lowest rock
+                if pos.y >= limit {
+                    return false;
+                } else {
+                    fill.insert(pos);
+                    return true;
+                }
+            }
+            Part::Part2 => {
+                // keep inserting until we fill the starting point
+                fill.insert(pos);
+                return pos.y != self.y;
+            }
+        }
+    }
+
+    fn tick_all(&self, fill: &HashSet<Filled>, part: &Part) -> HashSet<Filled> {
+        let mut hash = fill.clone();
+        let limit = hash.iter().fold(0, |acc, sand| max(acc, sand.y));
+
+        while self.tick(&mut hash, limit, part) {}
+
+        return hash;
+    }
+}
+
+fn main() {
+    let file = std::fs::read_to_string("../input.txt").expect("Unable to read file");
+
+    let init_state = parse_rocks(&file);
+    let rocks = init_state.len();
+    let sand = Filled { x: 500, y: 0 };
+
+    let p1_end_state = sand.tick_all(&init_state, &Part::Part1);
+    let p2_end_state = sand.tick_all(&init_state, &Part::Part2);
+
+    println!("Part 1 answer: {:?}", p1_end_state.len() - rocks);
+    println!("Part 2 answer: {:?}", p2_end_state.len() - rocks);
+}
