01d5e5f80df20d59a22456bf3b3ec53016f6cc80
chenson2018 <46805207+chenson2018@users.noreply.github.com>
2022-12-18 14:28:10 -0500

    single pass

--- 2022/07/rust/src/main.rs
+++ 2022/07/rust/src/main.rs
@@ -1,6 +1,5 @@
 use regex::Regex;
 use std::collections::HashMap;
-use std::iter::zip;
 
 fn main() {
     let file = std::fs::read_to_string("../input.txt").expect("Unable to read file");
@@ -26,7 +25,9 @@ fn main() {
         })
         .collect();
 
-    let full_paths = lines
+    let mut sizes: HashMap<Vec<String>, usize> = HashMap::new();
+
+    let _ = lines
         .iter()
         .scan(Vec::new(), |state, (name, size)| {
             if *size == 0 && name == ".." {
@@ -35,32 +36,22 @@ fn main() {
             if *size == 0 && name != ".." {
                 state.push(name.clone());
             }
+
+            let subdirs: Vec<Vec<String>> = state
+                .iter()
+                .scan(Vec::new(), |state, val| {
+                    state.push(val.clone());
+                    Some(state.clone())
+                })
+                .collect();
+
+            for dir in subdirs {
+                sizes.entry(dir).and_modify(|s| *s += size).or_insert(*size);
+            }
+
             Some(state.clone())
         })
         .collect::<Vec<Vec<String>>>();
-
-    let file_full_path: Vec<(Vec<String>, usize)> = zip(full_paths, lines)
-        .filter_map(|(path, (_, size))| match size {
-            0 => None,
-            _ => Some((path, size)),
-        })
-        .collect();
-
-    let mut sizes: HashMap<Vec<String>, usize> = HashMap::new();
-
-    for (path, size) in file_full_path {
-        let subdirs: Vec<Vec<String>> = path
-            .iter()
-            .scan(Vec::new(), |state, val| {
-                state.push(val.clone());
-                Some(state.clone())
-            })
-            .collect();
-
-        for dir in subdirs {
-            sizes.entry(dir).and_modify(|s| *s += size).or_insert(size);
-        }
-    }
 
     let p1_ans: usize = sizes.values().cloned().filter(|x| *x <= 100000).sum();
 
