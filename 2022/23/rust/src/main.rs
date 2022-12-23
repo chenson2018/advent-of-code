60fb94ddf061baca774bec715ce3b81b39d1be24
chenson2018 <46805207+chenson2018@users.noreply.github.com>
2022-12-23 02:10:36 -0500

    2022 Day 23

---
+++ 2022/23/rust/src/main.rs
@@ -0,0 +1,185 @@
+use std::collections::HashSet;
+
+#[derive(Debug, Eq, Hash, PartialEq, Clone, Copy)]
+struct Elf {
+    x: isize,
+    y: isize,
+}
+
+enum Dir {
+    N,
+    NE,
+    E,
+    SE,
+    S,
+    SW,
+    W,
+    NW,
+}
+
+impl Elf {
+    fn move_dir(&self, dir: &Dir) -> Self {
+        match dir {
+            Dir::N => Elf {
+                y: self.y + 1,
+                x: self.x,
+            },
+            Dir::NE => Elf {
+                y: self.y + 1,
+                x: self.x + 1,
+            },
+            Dir::E => Elf {
+                y: self.y,
+                x: self.x + 1,
+            },
+            Dir::SE => Elf {
+                y: self.y - 1,
+                x: self.x + 1,
+            },
+            Dir::S => Elf {
+                y: self.y - 1,
+                x: self.x,
+            },
+            Dir::SW => Elf {
+                y: self.y - 1,
+                x: self.x - 1,
+            },
+            Dir::W => Elf {
+                y: self.y,
+                x: self.x - 1,
+            },
+            Dir::NW => Elf {
+                y: self.y + 1,
+                x: self.x - 1,
+            },
+        }
+    }
+
+    fn adj_north(&self, elves: &HashSet<Elf>) -> bool {
+        elves.contains(&self.move_dir(&Dir::N))
+            || elves.contains(&self.move_dir(&Dir::NE))
+            || elves.contains(&self.move_dir(&Dir::NW))
+    }
+
+    fn adj_south(&self, elves: &HashSet<Elf>) -> bool {
+        elves.contains(&self.move_dir(&Dir::S))
+            || elves.contains(&self.move_dir(&Dir::SE))
+            || elves.contains(&self.move_dir(&Dir::SW))
+    }
+
+    fn adj_west(&self, elves: &HashSet<Elf>) -> bool {
+        elves.contains(&self.move_dir(&Dir::W))
+            || elves.contains(&self.move_dir(&Dir::NW))
+            || elves.contains(&self.move_dir(&Dir::SW))
+    }
+
+    fn adj_east(&self, elves: &HashSet<Elf>) -> bool {
+        elves.contains(&self.move_dir(&Dir::E))
+            || elves.contains(&self.move_dir(&Dir::NE))
+            || elves.contains(&self.move_dir(&Dir::SE))
+    }
+
+    fn tick(&self, elves: &HashSet<Elf>, n: usize) -> (Self, Self) {
+        let checks = [
+            self.adj_north(elves),
+            self.adj_south(elves),
+            self.adj_west(elves),
+            self.adj_east(elves),
+        ];
+
+        let dirs = [Dir::N, Dir::S, Dir::W, Dir::E];
+
+        let new = if !checks.iter().any(|x| *x) {
+            *self
+        } else if !checks[n % 4] {
+            self.move_dir(&dirs[n % 4])
+        } else if !checks[(n + 1) % 4] {
+            self.move_dir(&dirs[(n + 1) % 4])
+        } else if !checks[(n + 2) % 4] {
+            self.move_dir(&dirs[(n + 2) % 4])
+        } else if !checks[(n + 3) % 4] {
+            self.move_dir(&dirs[(n + 3) % 4])
+        } else {
+            *self
+        };
+
+        (*self, new)
+    }
+}
+
+fn tick_all(elves: &HashSet<Elf>, n: usize) -> HashSet<Elf> {
+    let tup_prev_next: Vec<(Elf, Elf)> = elves.iter().map(|elf| elf.tick(elves, n)).collect();
+    let maybe_next: Vec<Elf> = tup_prev_next
+        .clone()
+        .iter()
+        .map(|(_, nelf)| *nelf)
+        .collect();
+
+    tup_prev_next
+        .into_iter()
+        .map(|(original, new)| {
+            if maybe_next.iter().filter(|nelf| **nelf == new).count() == 1 {
+                new
+            } else {
+                original
+            }
+        })
+        .collect()
+}
+
+fn input_to_elves(grid: &Vec<Vec<char>>) -> HashSet<Elf> {
+    let mut res: HashSet<Elf> = HashSet::new();
+    let len = grid.len();
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
+            if *c == '#' {
+                res.insert(Elf {
+                    x: x as isize,
+                    y: (len - y) as isize, // doing this to make directions less confusing
+                });
+            }
+        }
+    }
+    res
+}
+
+fn score(elves: &HashSet<Elf>) -> usize {
+    let y_coor: Vec<isize> = elves.iter().map(|e| e.y).collect();
+    let x_coor: Vec<isize> = elves.iter().map(|e| e.x).collect();
+
+    (((y_coor.iter().max().unwrap() - y_coor.iter().min().unwrap() + 1)
+        * (x_coor.iter().max().unwrap() - x_coor.iter().min().unwrap() + 1)) as usize)
+        - elves.len()
+}
+
+fn main() {
+    let input: Vec<Vec<char>> = std::fs::read_to_string("../input.txt")
+        .expect("Unable to read file")
+        .lines()
+        .map(|l| l.chars().collect())
+        .collect();
+
+    let mut elves = input_to_elves(&input);
+    let mut round = 1;
+
+    while round <= 10 {
+        elves = tick_all(&elves, round - 1);
+        round += 1;
+    }
+
+    let p1_ans = score(&elves);
+    println!("Part 1 answer: {:#?}", p1_ans);
+
+    loop {
+        let next_iter = tick_all(&elves, round - 1);
+        if elves == next_iter {
+            break;
+        } else {
+            elves = next_iter;
+            round += 1;
+        }
+    }
+
+    println!("Part 2 answer: {}", round);
+}
