532d5d9eab8109edad5b2975489b89adc3575925
chenson2018 <46805207+chenson2018@users.noreply.github.com>
2023-03-05 16:14:26 -0500

    2015 Day 20 (part 2)

--- 2015/20/rust/src/main.rs
+++ 2015/20/rust/src/main.rs
@@ -6,26 +6,56 @@ fn presents_calc_p1(n: u32) -> u32 {
 
     // see https://math.stackexchange.com/questions/163245/finding-sum-of-factors-of-a-number-using-prime-factorization
 
-    let sum_factors = prime_tuples
-        .iter()
-        .fold(1, |acc, (factor, exponent)| {
-            let powers : u32 = (0..=*exponent).map(|exp| factor.pow(exp) ).sum();
-            acc * powers
-        });
+    let sum_factors = prime_tuples.iter().fold(1, |acc, (factor, exponent)| {
+        let powers: u32 = (0..=*exponent).map(|exp| factor.pow(exp)).sum();
+        acc * powers
+    });
 
     sum_factors * 10
+}
+
+fn all_factors(n: u32) -> Vec<u32> {
+    let factor_repr = Factorization::run(n);
+    let prime_tuples = factor_repr.prime_factor_repr();
+
+    prime_tuples
+        .iter()
+        .fold(vec![1], |acc: Vec<u32>, (factor, exponent)| {
+            acc.iter()
+                .map(|int| (0..=*exponent).map(move |exp| int * factor.pow(exp)))
+                .flatten()
+                .collect()
+        })
+}
+
+fn presents_calc_p2(n: u32) -> u32 {
+    let factors = all_factors(n);
+    11 * factors
+        .iter()
+        .filter(|x| (n as f64) / (**x as f64) <= 50.0)
+        .sum::<u32>()
 }
 
 fn main() {
     let input = 36000000;
 
-    let mut n = 1; 
-    let mut p1_presents = 0;
+    let mut n = 1;
+    let mut presents = 0;
 
-    while p1_presents < input {
-        p1_presents = presents_calc_p1(n);
+    while presents < input {
+        presents = presents_calc_p1(n);
         n += 1;
     }
 
-    println!("Part 1 answer: {}", n-1);
+    println!("Part 1 answer: {}", n - 1);
+
+    n = 1;
+    presents = 0;
+
+    while presents < input {
+        presents = presents_calc_p2(n);
+        n += 1;
+    }
+
+    println!("Part 2 answer: {}", n - 1);
 }
