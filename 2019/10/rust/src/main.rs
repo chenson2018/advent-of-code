4ce643eb06173505df7117c6afa6d4519c204ca9
Chris Henson <chenson@pop-os.localdomain>
2023-06-22 04:40:09 -0400

    sort into lines of sight earlier

--- 2019/10/rust/src/main.rs
+++ 2019/10/rust/src/main.rs
@@ -1,9 +1,10 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 use fraction::Fraction;
 use fraction::Sign;
 
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq, Debug)]
 enum Sight {
     Up,
     Down,
@@ -29,41 +30,51 @@ fn main() {
         .flatten()
         .collect();
 
-    let n_visible: &Vec<(isize, isize, usize)> = &mat[..]
+    let n_visible: &Vec<(isize, isize, HashMap<_, _>)> = &mat[..]
         .iter()
         .map(|(station_x, station_y)| {
-            let slopes: &HashSet<Sight> = &mat[..]
-                .iter()
-                .filter_map(|(asteroid_x, asteroid_y)| {
-                    if station_x == asteroid_x && station_y == asteroid_y {
-                        None
+            let mut groups: HashMap<Sight, Vec<(isize, isize)>> = HashMap::new();
+
+            for (asteroid_x, asteroid_y) in &mat[..] {
+                if station_x == asteroid_x && station_y == asteroid_y {
+                    continue;
+                };
+
+                let sight = if let Some(frac) = Fraction::new_generic(
+                    Sign::Plus,
+                    station_y - asteroid_y,
+                    station_x - asteroid_x,
+                ) {
+                    if asteroid_x > station_x {
+                        Sight::Right(frac)
                     } else {
-                        if let Some(frac) = Fraction::new_generic(
-                            Sign::Plus,
-                            station_y - asteroid_y,
-                            station_x - asteroid_x,
-                        ) {
-                            if asteroid_x > station_x {
-                                Some(Sight::Right(frac))
-                            } else {
-                                Some(Sight::Left(frac))
-                            }
-                        } else {
-                            if asteroid_x > station_x {
-                                Some(Sight::Up)
-                            } else {
-                                Some(Sight::Down)
-                            }
-                        }
+                        Sight::Left(frac)
                     }
-                })
-                .collect();
-            (*station_x, *station_y, slopes.len())
+                } else {
+                    if asteroid_x > station_x {
+                        Sight::Up
+                    } else {
+                        Sight::Down
+                    }
+                };
+
+                groups
+                    .entry(sight)
+                    .or_default()
+                    .push((*asteroid_x, *asteroid_y));
+            }
+            (*station_x, *station_y, groups)
         })
         .collect();
 
-    let (station_x, station_y, p1_ans) = n_visible.iter().max_by_key(|(_, _, n)| n).unwrap();
-    println!("Part 1 answer: {}", p1_ans);
+    let (station_x, station_y, groups) = n_visible
+        .iter()
+        .max_by_key(|(_, _, group)| group.len())
+        .unwrap();
+    println!("Part 1 answer: {:#?}", groups.len());
+
+    //    println!("Station: {}, {}", station_x, station_y);
+    //    println!("{:#?}", groups);
 
     // TODO part 2...
     // get the  (x,y) -> slope relative to the station
