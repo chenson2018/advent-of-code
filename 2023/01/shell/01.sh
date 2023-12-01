0a4ef4d17ba632a899e8a19bda1c4f3006829927
Chris Henson <chrishenson.net@gmail.com>
2023-12-01 06:46:38 -0500

    no need for rev

--- 2023/01/shell/01.sh
+++ 2023/01/shell/01.sh
@@ -1,11 +1,10 @@
 in="../input.txt"
-pat="(\d).*"
-paste <(pcregrep -o1 $pat $in) <(rev $in | pcregrep -o1 $pat) -d "" | paste -sd+ | bc
+paste <(pcregrep -o1 "(\d).*" $in) <(pcregrep -o1 ".*(\d)" $in) -d "" | paste -sd+ | bc
 
 nums="one|two|three|four|five|six|seven|eight|nine"
 pat1="(${nums}|\d).*"
-pat2="($(rev <<<$nums)|\d).*"
-paste <(pcregrep -o1 $pat1 $in) <(rev $in | pcregrep -o1 $pat2 | rev) -d "" | paste -sd+ |
+pat2=".*(${nums}|\d)"
+paste <(pcregrep -o1 $pat1 $in) <(pcregrep -o1 $pat2 $in) -d "" | paste -sd+ |
 	sed 's/one/1/g' |
 	sed 's/two/2/g' |
 	sed 's/three/3/g' |
