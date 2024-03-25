52110275e43e1223aa9fc56cd9889c8e9528ad72
Chris Henson <chrishenson.net@gmail.com>
2024-03-25 15:56:05 -0400

    currying

--- 2015/19/haskell/19.hs
+++ 2015/19/haskell/19.hs
@@ -3,19 +3,19 @@ import Data.Maybe
 import Parsing
 
 -- surprisingly tricky to make this work for variable lengths
-aux :: String -> String -> String -> String -> [String]
-aux _ [] _ _ = []
-aux prev front old new
+aux :: String -> String -> (String, String) -> [String]
+aux _ [] _ = []
+aux prev front (old, new)
   | old `isPrefixOf` front = (reverse prev ++ new ++ pastPrefix) : continue
   | otherwise = continue
   where
     pastPrefix = drop (length old) front
-    continue = aux (head front : prev) (drop 1 front) old new
+    continue = aux (head front : prev) (drop 1 front) (old, new)
 
 rmdups :: (Ord a) => [a] -> [a]
 rmdups = map head . group . sort
 
-replace :: String -> String -> String -> [String]
+replace :: String -> (String, String) -> [String]
 replace = aux ""
 
 chemMap :: Parser (String, String)
@@ -25,10 +25,10 @@ chemMap = do
   new <- many letter
   return (old, new)
 
-p1 :: String -> [(String, String)] -> Int
-p1 s = length . rmdups . concatMap (uncurry (replace s))
+iter :: [(String, String)] -> [String] -> [String]
+iter mappings xs = concat $ replace <$> xs <*> mappings
 
 main = do
   (input : _ : mappings_raw) <- reverse . lines <$> readFile "../input.txt"
   let mappings = map fst $ fromJust $ mapM (parse chemMap) mappings_raw
-  print (p1 input mappings)
+  print (length $ rmdups $ iter mappings [input])
