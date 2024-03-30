f2321801cb6fec1e4dde98dc1ab67dbae9f89ac5
Chris Henson <chrishenson.net@gmail.com>
2024-03-30 11:24:58 -0400

    part 2

--- 2021/09/haskell/09.hs
+++ 2021/09/haskell/09.hs
@@ -1,17 +1,31 @@
 import Control.Applicative
 import Data.Bifunctor
+import Data.List
 import qualified Data.Map as M
 import Data.Maybe
 
+-- remove duplicates, from SO
+rmdups :: (Ord a) => [a] -> [a]
+rmdups = map head . group . sort
+
+-- for any type of 2s lists, create a map of coordinates
 grid :: [[a]] -> M.Map (Int, Int) a
 grid = M.fromList . concat . zipWith zip [[(x, y) | x <- [0 ..]] | y <- [0 ..]]
 
-surrounding :: M.Map (Int, Int) Int -> (Int, Int) -> [Int]
-surrounding m key = vals
+-- get key/values to N/S/E/W of a key
+surroundingWithKey :: M.Map (Int, Int) Int -> (Int, Int) -> [((Int, Int), Int)]
+surroundingWithKey m key = ret
   where
     directions = [(id, (+ 1)), ((+ 1), id), (subtract 1, id), (id, subtract 1)]
     keys = map (($ key) . uncurry bimap) directions
-    vals = mapMaybe (`M.lookup` m) keys
+    key_vals = zip keys $ map (`M.lookup` m) keys
+    f (_, Nothing) = Nothing
+    f (a, Just b) = Just (a, b)
+    ret = mapMaybe f key_vals
+
+-- get values to N/S/E/W of a key
+surrounding :: M.Map (Int, Int) Int -> (Int, Int) -> [Int]
+surrounding m key = map snd $ surroundingWithKey m key
 
 checkLow :: M.Map (Int, Int) Int -> (Int, Int) -> Int -> Bool
 checkLow m key val = all (val <) $ surrounding m key
@@ -19,8 +33,19 @@ checkLow m key val = all (val <) $ surrounding m key
 p1 :: M.Map (Int, Int) Int -> Int
 p1 m = sum $ (+ 1) <$> M.filterWithKey (checkLow m) m
 
+iter :: M.Map (Int, Int) Int -> [((Int, Int), Int)] -> [((Int, Int), Int)]
+iter m = concatMap (step m)
+  where
+    step m (key, value) = filter (((&&) <$> (> value) <*> (/= 9)) . snd) $ surroundingWithKey m key
+
+basins :: M.Map (Int, Int) Int -> [((Int, Int), Int)] -> [((Int, Int), Int)]
+basins m xs = rmdups $ concat $ takeWhile (/= []) $ iterate (iter m) xs
+
+p2 :: M.Map (Int, Int) Int -> Int
+p2 m = product $ take 3 $ sortBy (flip compare) $ map (length . basins m . (: [])) $ M.toList $ M.filterWithKey (checkLow m) m
+
 main =
   do
-    input :: M.Map (Int, Int) Int <- grid . (map . map) (read . (: [])) . lines <$> readFile "../test.txt"
     input :: M.Map (Int, Int) Int <- grid . (map . map) (read . (: [])) . lines <$> readFile "../input.txt"
     print (p1 input)
+    print (p2 input)
