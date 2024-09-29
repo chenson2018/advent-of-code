35373f457e44e762cd1edf953f40867987b5462c
Chris Henson <chrishenson.net@gmail.com>
2024-09-29 01:23:56 -0400

    cleaner

--- 2018/07/haskell/07.hs
+++ 2018/07/haskell/07.hs
@@ -1,44 +1,42 @@
-import qualified Data.Map as M
-import Parsing
+{-# OPTIONS_GHC -Wno-x-partial #-}
+
 import Data.List
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Parsing
 
 extractFirst f xs = aux f xs []
   where
     aux :: (a -> Bool) -> [a] -> [a] -> Maybe (a, [a])
     aux _ [] _ = Nothing
-    aux f (x:r) l
+    aux f (x : r) l
       | f x = Just (x, reverse l ++ r)
-      | otherwise = aux f r (x:l)
+      | otherwise = aux f r (x : l)
 
-line = 
+line =
   do
-  symbol "Step"
-  l <- upper
-  symbol "must be finished before step"
-  r <- upper
-  return (l, r)
+    symbol "Step"
+    l <- upper
+    symbol "must be finished before step"
+    r <- upper
+    return (l, r)
 
-buildMap xs = aux (sortBy (flip compare) xs) M.empty 
+mapAndStart [] m ls rs = (m, ls S.\\ rs)
+mapAndStart ((l, r) : xs) m ls rs = mapAndStart xs new_m (S.insert l ls) (S.insert r rs)
   where
-   aux [] m = m
-   aux ((r,l):xs) m = aux xs new_m
-     where 
-       up Nothing = [r]
-       up (Just xs) = (r:xs)
-       new_m = M.alter (Just . up) l m
+    up Nothing = [l]
+    up (Just xs) = (l : xs)
+    new_m = M.alter (Just . up) r m
 
-rmdups :: (Ord a) => [a] -> [a]
-rmdups = map head . group . sort
-
-addStart m = M.assocs $ foldl (\m' k -> M.insert k "@" m') m ((rmdups $ concat $ M.elems m) \\ (M.keys m))
+buildMap xs = M.assocs $ uncurry (foldl (\m' k -> M.insert k "@" m')) $ mapAndStart (sortBy (flip compare) xs) M.empty S.empty S.empty
 
 iter open [] = open
-iter open remain = 
-  case extractFirst (all (`elem` open). snd) remain of
-    Just (no, remain') -> iter (fst no:open) remain'
+iter open remain =
+  case extractFirst (all (`elem` open) . snd) remain of
+    Just (no, remain') -> iter (fst no : open) remain'
 
 main = do
-  input <- parseListJust line . lines <$> readFile "../test.txt"
+  -- input <- parseListJust line . lines <$> readFile "../test.txt"
   input <- parseListJust line . lines <$> readFile "../input.txt"
-  let m = addStart $ buildMap input
+  let m = buildMap input
   print $ tail $ reverse $ iter "@" m
