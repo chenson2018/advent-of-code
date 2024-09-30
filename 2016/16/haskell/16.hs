5c5cdf6aa048cafabe070ed2851c44e9e21478c4
Chris Henson <chrishenson.net@gmail.com>
2024-09-29 23:54:05 -0400

    as numbers

--- 2016/16/haskell/16.hs
+++ 2016/16/haskell/16.hs
@@ -1,15 +1,15 @@
 import Data.List
 
-swap '0' = '1'
-swap '1' = '0'
+swap 0 = 1
+swap 1 = 0
 
-iter a = a ++ '0' : (map swap . reverse) a
+iter a = a ++ 0 : (map swap . reverse) a
 
 checksum xs
   | length xs `mod` 2 == 1 = xs
   | otherwise = checksum $ aux xs
   where
-    aux (a : b : xs) = (if a == b then '1' else '0') : aux xs
+    aux (a : b : xs) = (if a == b then 1 else 0) : aux xs
     aux [] = []
 
 p1 init len = do
@@ -17,6 +17,6 @@ p1 init len = do
   return $ checksum $ take len $ filled
 
 main = do
-  let input = "10001110011110000"
+  let input = map (read . (: [])) "10001110011110000"
   print $ p1 input 272
   print $ p1 input 35651584
