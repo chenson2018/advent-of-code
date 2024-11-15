2860274796296d9395af5f7c6ae97c385a748dba
Chris Henson <chrishenson.net@gmail.com>
2024-11-14 19:06:27 -0500

    2019 Day 5 (Lean)

--- 2019/LeanIntcode/LeanIntcode/Intcode.lean
+++ 2019/LeanIntcode/LeanIntcode/Intcode.lean
@@ -1,52 +1,127 @@
 import LeanIntcode.Tactic
 
+/-- A structure representing an Intcode virtual machine -/
 structure Intcode where
-  data : Array Nat
+  /-- an array of integer-valued memory -/
+  data : Array Int
+  /-- the current instruction pointer -/
   ptr : Nat
+  /-- is the Vm halted? -/
   halted : Bool
-deriving Repr
+  /-- a record of any outputs made (in reverse order) -/
+  output : List Int
+  /-- values for precomputed input -/
+  input : List Int
+  /-- option to supress printing -/
+  silent : Bool
+deriving Repr, Inhabited
 
+/-- Iterate a monad until some condtion is met -/
 partial def bindUntil [Monad m] (cond : A → Bool) (iter : A → m A) (a : A) : m A :=
   if cond a then pure a else iter a >>= bindUntil cond iter
 
-inductive Opcode where | Add | Mult | Halt deriving Repr
+/-- lift any optional type into OptionT -/
+def hoistOption [Monad m] : Option T → OptionT m T := OptionT.mk ∘ pure
+
+inductive Opcode where 
+| Add 
+| Mult 
+| Halt 
+| In 
+| Out 
+| JumpTrue 
+| JumpFalse 
+| Less 
+| Equal 
+deriving Repr
+
+inductive Mode where 
+| Imm 
+| Pos 
+deriving Repr, DecidableEq
 
 open Opcode in
-def Nat.toOpcode? (val : Nat) : Option Opcode :=
+/-- convert an integer into an opcode -/
+def Int.toOpcode? (val : Int) : Option Opcode :=
   match val with
   | 1  => some Add
   | 2  => some Mult
+  | 3  => some In
+  | 4  => some Out
+  | 5  => some JumpTrue
+  | 6  => some JumpFalse
+  | 7  => some Less
+  | 8  => some Equal
   | 99 => some Halt
   | _  => none
 
+/-- number of arguments for each opcode -/
 def Opcode.len (opcode : Opcode) : Nat :=
   match opcode with
-  | Add | Mult => 3
-  | Halt       => 0
+  | Halt                      => 0
+  | In | Out                  => 1
+  | JumpTrue | JumpFalse      => 2
+  | Add | Mult | Equal | Less => 3
+
+/-- mode of the nth argument of an opcode -/
+def Int.getMode  (val: Int) (n : Nat) := if 0 = val / 10^(n+2) % 10 then Mode.Pos else Mode.Imm
 
 namespace Intcode
 
-def new (data : Array Nat) : Intcode := {data, ptr := 0, halted := False}
+/-- initialize an Intcode VM with some defaults -/
+def new (data : Array Int) (input := @List.nil Int) (silent := false) : Intcode := {data, ptr := 0, halted := False, output := [], input, silent}
 
-def hoistOption [Monad m] : Option T → OptionT m T := OptionT.mk ∘ pure
-
--- TODO: setting values can still fail here
-
+/-- execute one opcode -/
 def tick (vm : Intcode) : OptionT IO Intcode := do
+  -- read the raw opcode
   let raw_opcode ← hoistOption (vm.data.get? vm.ptr)
-  let opcode ← hoistOption raw_opcode.toOpcode?
+
+  -- the last two digits are the opcode
+  let opcode ← hoistOption (raw_opcode % 100).toOpcode?
+
+  -- get the possible immediate values
   -- TODO: I use zero as a default value since this is eagerly evaluated, is there a better way?
   let imm1 ← hoistOption (if 1 ≤ opcode.len then (vm.data.get? (vm.ptr + 1)) else some 0)
   let imm2 ← hoistOption (if 2 ≤ opcode.len then (vm.data.get? (vm.ptr + 2)) else some 0)
   let imm3 ← hoistOption (if 3 ≤ opcode.len then (vm.data.get? (vm.ptr + 3)) else some 0)
-  let pos1 ← hoistOption (if 1 ≤ opcode.len then (vm.data.get? imm1) else some 0)
-  let pos2 ← hoistOption (if 2 ≤ opcode.len then (vm.data.get? imm2) else some 0)
-  let pos3 ← hoistOption (if 3 ≤ opcode.len then (vm.data.get? imm3) else some 0)
-  match opcode with
-  | Opcode.Add  => pure {vm with data := vm.data.set! imm3 (pos1 + pos2), ptr := vm.ptr + opcode.len + 1}
-  | Opcode.Mult => pure {vm with data := vm.data.set! imm3 (pos1 * pos2), ptr := vm.ptr + opcode.len + 1}
-  | Opcode.Halt => pure {vm with halted := True}
+ 
+  -- TODO: better conversion here than .natAbs ?
+  let val1 ← hoistOption (if raw_opcode.getMode 0 = Mode.Imm then imm1 else if 1 ≤ opcode.len then (vm.data.get? imm1.natAbs) else some 0)
+  let val2 ← hoistOption (if raw_opcode.getMode 1 = Mode.Imm then imm2 else if 2 ≤ opcode.len then (vm.data.get? imm2.natAbs) else some 0)
 
+  -- a utility for the simple opcodes
+  let advance := vm.ptr + opcode.len + 1
+  let try_set (idx val : Int) (advance := advance) (input := []) := do
+    let data ← hoistOption 
+      (match Nat.decLt idx.natAbs vm.data.size with
+        | isTrue h => some (vm.data.set ⟨idx.natAbs, h⟩ val)
+        | isFalse _ => none)
+    pure {vm with ptr := advance, data, input}
+
+  match opcode with
+  | Opcode.Add  => try_set imm3 (val1 + val2)
+  | Opcode.Mult => try_set imm3 (val1 * val2)
+  | Opcode.In   => 
+      let (val,input) ← match vm.input with
+        -- case for precomputed input
+        | val :: input => pure (val,input)
+        -- otherwise actually read from stdin
+        | [] => 
+          let stdin ← IO.getStdin
+          let input ← stdin.getLine
+          let val ← hoistOption input.trim.toInt?
+          pure (val,[])    
+      try_set imm1 val (input := input)
+  | Opcode.Out  => 
+      if vm.silent then pure () else println! s!"Intcode output: {val1}"
+      pure {vm with ptr := advance, output := val1 :: vm.output}
+  | Opcode.Halt => pure {vm with halted := True}
+  | Opcode.JumpTrue => pure {vm with ptr := if val1 ≠ 0 then val2.natAbs else advance}
+  | Opcode.JumpFalse => pure {vm with ptr := if val1 = 0 then val2.natAbs else advance}
+  | Opcode.Less => try_set imm3 (if val1 < val2 then 1 else 0)
+  | Opcode.Equal => try_set imm3 (if val1 = val2 then 1 else 0)
+
+/-- executes opcodes until VM halts -/
 def run := bindUntil Intcode.halted tick
 
 end Intcode
--- 2019/LeanIntcode/Main.lean
+++ 2019/LeanIntcode/Main.lean
@@ -1,7 +1,7 @@
 import LeanIntcode
 
 -- Day 2
-def noun_verb (data : Array Nat) (noun verb : Nat) := do
+def noun_verb (data : Array Int) (noun verb : Nat) := do
   let data := data.set! 1 noun
   let data := data.set! 2 verb
   let vm := Intcode.new data
@@ -10,16 +10,33 @@ def noun_verb (data : Array Nat) (noun verb : Nat) := do
 
 def day2 : IO Unit := do
   let input ← IO.FS.readFile "../02/input.txt"
-  let data := Array.map String.toNat! (List.toArray (input.trim.splitOn (sep := ",")))
+  let data := Array.map String.toInt! (List.toArray (input.trim.splitOn (sep := ",")))
   -- part 1
   let p1 ← noun_verb data 12 2
   -- precomputed part 2
   let (p2_noun, p2_verb) := (76, 10)
   let p2 ← noun_verb data p2_noun p2_verb
+  assert! p1 = some 3765464
   assert! p2 = some 19690720
   println! s!"Day 2, Part 1 answer : {p1.get!}"
   println! s!"Day 2, Part 2 answer : {p2_noun * 100 + p2_verb}"
   println! ""
 
+def day5 : IO Unit := do
+  let input ← IO.FS.readFile "../05/input.txt"
+  let data := Array.map String.toInt! (List.toArray (input.trim.splitOn (sep := ",")))
+  let p1 ← (Intcode.new (input := [1]) (silent := true) data).run
+  let p2 ← (Intcode.new (input := [5]) (silent := true) data).run
+  assert! p1.get!.output.reverse = [0,0,0,0,0,0,0,0,0,7839346]
+  assert! p2.get!.output.reverse = [447803]
+  println! s!"Day 5, Part 1 answer : {p1.get!.output.head!}"
+  println! s!"Day 5, Part 2 answer : {p2.get!.output.head!}"
+  println! ""
+
 def main : IO Unit := do
   day2
+  day5
+  -- let vm := Intcode.new #[3,0,4,0,99]
+  -- println! (repr vm)
+  -- let vm ← vm.run
+  -- println! (repr vm)
