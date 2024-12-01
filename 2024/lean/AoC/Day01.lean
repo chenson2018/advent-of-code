9f721a4732116fed7508870e3581e472336761be
Chris Henson <chrishenson.net@gmail.com>
2024-12-01 03:01:42 -0500

    style

--- 2024/lean/AoC/Day01.lean
+++ 2024/lean/AoC/Day01.lean
@@ -15,8 +15,9 @@ def part1 (args : List String) : IO Unit := do
   let [filename] := args | throw <| IO.userError "Expecting one argument, the input file"
   let lines ← IO.FS.lines filename
   let parsed ← lines.mapM (IO.ofExcept ∘ parse.run)
-  let l := (parsed.map (ofNat ∘ fst)).qsort (Ordering.isLT $ compare · ·)
-  let r := (parsed.map (ofNat ∘ snd)).qsort (Ordering.isLT $ compare · ·)
+  let (l,r) := parsed.unzip
+  let l := (l.map ofNat).qsort (Ordering.isLT $ compare · ·)
+  let r := (r.map ofNat).qsort (Ordering.isLT $ compare · ·)
   let diffs := Array.zipWith l r (natAbs $ · - ·)
   let ans := diffs.foldl (· + ·) 0
   assert! ans = 1151792
@@ -24,7 +25,7 @@ def part1 (args : List String) : IO Unit := do
 
 -- array to counts of elements
 def Array.counts {α : Type} [BEq α] [Hashable α] (xs : Array α) : Std.HashMap α Nat := 
-  xs.foldl (fun m a => m.insert a (m.getD a 0 + 1)) Std.HashMap.empty
+  xs.foldl (λ m a => m.insert a $ m.getD a 0 + 1) Std.HashMap.empty
 
 open Prod Int in
 @[aoc_main day_01_p2]
@@ -32,10 +33,8 @@ def part2 (args : List String) : IO Unit := do
   let [filename] := args | throw <| IO.userError "Expecting one argument, the input file"
   let lines ← IO.FS.lines filename
   let parsed ← lines.mapM (IO.ofExcept ∘ parse.run)
-  let l := parsed.map fst
-  let r := parsed.map snd
-  let r_counts := r.counts
-  let scores := l.map (fun x => x * r_counts.getD x 0)
+  let (l,r) := parsed.unzip
+  let scores := l.map (λ x => x * r.counts.getD x 0)
   let ans := scores.foldl (· + ·) 0
   assert! ans = 21790168
   println! s!"Part 2 answer: {ans}"
