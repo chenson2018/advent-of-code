ec732c225317e8582a59c9fbe43affbd4be56aed
Chris Henson <chrishenson.net@gmail.com>
2024-12-05 12:44:04 -0500

    no lists!

--- 2024/lean/AoC/Day05.lean
+++ 2024/lean/AoC/Day05.lean
@@ -5,6 +5,8 @@ open Std.Internal.Parsec
 open Std
 open Std.HashMap
 open Std.HashSet
+open Function
+open Prod
 
 def Array.middle (xs : Array α) : Option α := xs[xs.size / 2]?
 
@@ -34,15 +36,6 @@ def parse_update : Parser (Array Nat) := many (do let x ← nat; _ ← many (ski
 def rules_map (rules : Array (Nat × Nat)) : HashMap Nat (HashSet Nat) := 
   rules.foldl (λ m (lower, upper) => m.alter lower (λ hs => insert (hs.getD empty) upper)) empty
 
-def valid_update? := aux [] where
-  aux (prev : List Nat) (map : HashMap Nat (HashSet Nat)) (xs : List Nat) : Bool :=
-   match xs with
-   | [] => true
-   | hd :: tl =>
-       let set := map.getD hd empty
-       let check_current := prev.all (not ∘ set.contains)
-       check_current ∧ aux (hd :: prev) map tl
-
 def rules_qsort (map : HashMap Nat (HashSet Nat)) (xs : Array Nat) : Array Nat := 
     xs.qsort (λ a b => map[a]?.elim false (contains · b))
 
@@ -55,12 +48,14 @@ def day_05 (args : List String) : IO Unit := do
   let rules ← text.filter (flip String.contains '|') |>.mapM parse_rule.run |> IO.ofExcept
   let map := rules_map rules
 
-  let (valid,invalid) := updates.partition (valid_update? map ∘ Array.toList) 
+  let sorted := updates.map (λ u => (u,rules_qsort map u))
+  let (valid,invalid) := sorted.partition (uncurry (· == ·))
+  let sum_sort_middle := (·|>.map (flip Option.getD 0 ∘ Array.middle ∘ snd) |>.foldl (·+·) 0)
 
-  let p1_ans := valid |>.map (flip Option.getD 0 ∘ Array.middle) |>.foldl (·+·) 0
+  let p1_ans := sum_sort_middle valid
   assert! p1_ans = 5166
   println! s!"Part 1 answer: {p1_ans}"
 
-  let p2_ans := invalid |>.map (rules_qsort map) |>.map (flip Option.getD 0 ∘ Array.middle) |>.foldl (·+·) 0
+  let p2_ans := sum_sort_middle invalid
   assert! p2_ans = 4679
   println! s!"Part 2 answer: {p2_ans}"
